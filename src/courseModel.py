4c7746a74d07fde29a71778c76cdfe552555bcf8
Kaleb <kaleblianggg@icloud.com>
2022-10-26 14:26:06 -0500

    new file:   src/courseModel.py new file:   src/eventModel.py

---
+++ src/courseModel.py
@@ -0,0 +1,9 @@
+# author: Kaleb Liang
+
+class courseModel:
+    def __init__(self, instructor, location, section, department, courseID):
+        self.instructor = instructor
+        self.location = location
+        self.section = section
+        self.department = department
+        self.courseID = courseID
---
+++ src/eventModel.py
@@ -0,0 +1,17 @@
+# author: Kaleb Liang
+
+class eventModel:
+    def __init__(self, eventName, eventTime, eventDescription, calendarID):
+        self.eventName = eventName
+        self.eventTime = eventTime
+        self.eventDescription = eventDescription
+        self.calendarID = calendarID
+
+    def event():
+        pass
+
+    def getCalendar(id):
+        pass
+
+    def mergeEvents():
+        pass
