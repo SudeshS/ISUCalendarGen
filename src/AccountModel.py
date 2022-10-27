dd7084b42b2a64d32868c50c340fc08da88e509f
Jakob Syvertsen <jakobosyvertsen@gmail.com>
2022-10-27 18:59:00 -0500

    Added comments to the classes for the deliverable.

--- src/AccountModel.py
+++ src/AccountModel.py
@@ -1,10 +1,17 @@
+#Author: Jakob Syvertsen
+
+"""
+This is the AccountModel class, which extends the User class.
+It is the main class for dealing with User accounts.
+In particular, it handles viewing saved calendars and editing saved calendars.
+"""
 class AccountModel:
-    def __init__(self, username, password):
+    def __init__(self, username, password): #Declaring the class
         self.username = username
         self.password = password
 
-    def viewAllSavedCalendars():
+    def viewAllSavedCalendars(): #shows the User all currently saved calendars
         pass
 
-    def editSavedCalendar():
+    def editSavedCalendar(): #allows the User to edit a saved calendar
         pass
--- src/UserModel.py
+++ src/UserModel.py
@@ -1,17 +1,24 @@
+#author: Jakob Syvertsen
+
+"""
+This is the UserModel class.
+It is the main class for everything related to the User directly, such as uploading and downloading calendars.
+The UserModel class is also responsible for checking if a User has an account. 
+"""
 class UserModel:
-    def __init__(self, userID, isGuest):
+    def __init__(self, userID, isGuest): #Declaring the class
         self.userID = userID
         self.isGuest = isGuest
 
-    def getID():
+    def getID(): #gets the User ID of this User
         pass
 
-    def isGuest():
+    def isGuest(): #returns whether or not the current User is a guest (doesn't have an account)
         pass
 
-    def uploadCalendar():
+    def uploadCalendar(): #uploads a calendar file to the program
         pass
 
-    def downloadCalendar():
+    def downloadCalendar(): #downloads a calendar file from the program
         pass
     
