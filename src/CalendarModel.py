3a309b5e93ef6b47fb677ebf1b5e8014b09d8f46
Xavier <arriagaxavier0@gmail.com>
2022-11-10 20:25:48 -0600

    Some parameter changes to Cal.Model and Cal.Prev.

--- src/CalendarModel.py
+++ src/CalendarModel.py
@@ -4,42 +4,59 @@ This is the CalendarModel Class, aka Calendar.
 It is the main class for creating, updating, and removing events.
 It also deals with generating the ICSFile and generating a preview
 """
+import eventModel
+import CalendarPreview 
 
 class CalendarModel:
-    def __init__(cal, calendarID, calendarName): #Declaring the class
-        cal.calendarID = calendarID
-        cal.calendarName = calendarName 
+    def __init__(self, calendarID, calendarName): #Declaring the class
+        self.calendarID = calendarID
+        self.calendarName = calendarName 
 
     def checkCalendarFormat(): #checks if the calendar is formated correctly
+        #will read in a file to ensure that the calendar that will be
+        #generated is correctly formated.
         return bool
 
-    def addEvents(): #will add an event and return nothing
+    def addEvents(self, eName, eTime, eDesc): #will add an event and return nothing
+        #eName = input("Enter event Name: ")
+        #eTime = input("Enter time slot (like xx:xx to xx:xx): ")
+        #eDesc = input("Enter a short description on your event: ")
+        self.event = eventModel(eName, eTime, eDesc, self.getCalendarID())
         pass
 
-    def updateEvent(): #updates an event chosen by the user (might need to add an argument for that)
+    def updateEvent(self, newName, newTime, newDesc): #updates an event chosen by the user (might need to add an argument for that) 
+        #newName = input("Enter event Name: ")
+        #newTime = input("Enter time slot (like xx:xx to xx:xx): ")
+        #newDesc = input("Enter a short description on your event: ")
+        self.event = eventModel(newName, newTime, newDesc, self.getCalendarID())
+        self.event.editEvent()
         pass
+    #although it might not matter for right now, actual users might want to have the ability to only edit one piece of info
 
-    def removeEvents(): #removes the event or events given (may have to limit it to one event per call and will need arguments)
+    def removeEvents(self, event): #removes the event or events given (may have to limit it to one event per call)
         pass
 
     def generateICSFile(): #generates the ICSFile to be exported/downloaded (may need to return or print a string)
+        #should call the ics_generator.py file?
         pass
 
-    def generatePreview(): #gives a preview of what the calendar will look like (will need to present an image or text based image of some kind)
+    def generatePreview(self, user): #gives a preview of what the calendar will look like (will need to generate calendar preview)
+        self.preview = CalendarPreview(self.getCalendarID(), user)
+        self.preview.display()
         pass
 
     #Getters
-    def getCalendarID(cal):
-        return cal.calendarID
+    def getCalendarID(self):
+        return self.calendarID
 
-    def getCalendarName(cal):
-        return cal.calendarName
+    def getCalendarName(self):
+        return self.calendarName
 
     #Setters
-    def setCalendarID(cal, calID):
-        cal.calendarID = calID
+    def setCalendarID(self, calID):
+        self.calendarID = calID
         pass
 
-    def setCalendarName(cal, calName):
-        cal.calendarName = calName
+    def setCalendarName(self, calName):
+        self.calendarName = calName
         pass
