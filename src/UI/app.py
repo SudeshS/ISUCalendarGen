0e3d1cc920cc33ea2027f46914556c83a568c9d3
Gordon-Blankenship <gordon.l.blankenship@gmail.com>
2022-11-30 21:48:49 -0600

    merged app.py s

--- src/UI/app.py
+++ src/UI/app.py
@@ -1,28 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+# Author: Xavier Arriaga, Gordon (Tre) Blankenship
+from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_login import LoginManager
 import os
 from os.path import join, dirname, realpath
 
-# login_manager = LoginManager()
-app = Flask(__name__)
-# login_manager.init_app(app)
+##from iCalendar import Calendar, Event
+#from wtforms import Form, BooleanField, StringField, PasswordField, validators
+# ^ for validation if we have time for it, but this requires pip install Flask Flask-WTF
 
-# enable debugging mode
+# This is the url that our server runs on
+# host_URL = 
+
+#### NOTE: might have to rename to app.py for it to run properly ####
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cc30d0a491daf6a4ba282e9ea5f9dcfc994cb4b86d66f531'
+
+# Messages is just a name, I was gonna switch it to classes
+# but I couldn't get it to work for the time being, so keep it for now
+# unless you know how to fix it
+messages = [
+]
+
+# debug mode toggle comment
 app.config["DEBUG"] = True
 
-# Upload Folder
+# Upload folder
+# this is where things will be stored locally until they can
+# be integrated into the database
 UPLOAD_FOLDER = app.static_folder
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER+'\\uploads\\'
 
-
-# Root URL - What the user connects to
-@app.route('/')
-def index():
+# Upload page rendering
+@app.route('/upload/')
+def upload():
     # Set the upload HTML template '\templates\index.html'
     return render_template('upload.html')
 
 # Uploading files
-@app.route("/", methods=['POST'])
+@app.route('/upload/', methods=['POST'])
 def uploadFiles():
     # get the uploaded file
     uploaded_file = request.files['file']
@@ -31,7 +46,50 @@ def uploadFiles():
         # set the file path
         uploaded_file.save(file_path)
         # save the file
-    return redirect(url_for('index'))
+    return redirect(url_for('upload'))
 
+# Create page rendering
+@app.route('/create/')
+def index():
+    return render_template('index.html', messages=messages)
+    # these files are key to this working
+
+# Create handles the GET-ing of information from the form
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        summary = request.form['Summary']
+        DTStart = request.form['DTSTART']  # start date
+        StartTime = request.form['StartTime']  # start time
+        DTEnd = request.form['DTEND']
+        Duration = request.form['Duration']
+        UNTIL = request.form['UNTIL']
+        BYDAY = request.form['BYDAY']
+        Description = request.form['Description']
+        Location = request.form['Location']
+        # DTStamp will be down on backend, will be time the event is created
+
+        if not summary:
+            flash('Class Name is required!')
+        elif not DTStart:
+            flash('Start Date is required!')
+        elif not StartTime:
+            flash('Start Time is required')
+        elif not DTEnd:
+            flash('End Time is required')
+        elif not Duration:
+            flash('Duration is required')
+        elif not UNTIL:
+            flash('UNTIL is required')
+        elif not BYDAY:
+            flash('BYDAY is required')
+        else:
+            messages.append({'Summary': summary, 'DTSTART': DTStart, 'DTEND': DTEnd, 'Duration': Duration,
+                            'UNTIL': UNTIL, 'BYDAY': BYDAY, 'Description': Description, 'Location': Location})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+# Location should be kept not required alongside some others likely (description?)
 if (__name__ == '__main__'):
-    app.run(port = 5000)
+    app.run(port=5000)
