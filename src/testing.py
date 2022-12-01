2e5c74bf3f55969ca71b7ac7dbf7653a0db86583
Kaleb <kaleblianggg@icloud.com>
2022-12-01 11:36:40 -0600

    added some prints and import

--- src/testing.py
+++ src/testing.py
@@ -1,24 +1,30 @@
-from CalendarModel import parse_cal, generateICSFile, checkCalendarFormat
+from CalendarModel import *
+
+# test 1
 
 
-def test_no_events():   # test with no events
+def test_guest_no_events():   # guest test generation with no events
     f_path = 'data/input_isu_cal_i.ics'
-    test_cal = parse_cal(f_path)
-    if checkCalendarFormat(test_cal):
-        generateICSFile(test_cal)
+    test_cal = CalendarModel.parse_cal(f_path)
+    if CalendarModel.checkCalendarFormat(test_cal):
+        CalendarModel.generateICSFile(test_cal)
+        print("Test 1 Successful")
     else:
         print("Format Error")
 
 
-def test_with_events():  # test with four events
+# test 2
+def test_guest_with_events():  # guest test generation with four events
     f_path = 'data/input_isu_cal_i.ics'
-    test_cal = parse_cal(f_path)
-    if checkCalendarFormat(test_cal):
-        generateICSFile(test_cal)
+    test_cal = CalendarModel.parse_cal(f_path)
+    if CalendarModel.checkCalendarFormat(test_cal):
+        CalendarModel.generateICSFile(test_cal)
+        print("Test 2 Successful")
     else:
         print("Format Error")
 
 
 if __name__ == "__main__":
-    test_no_events()
-    test_with_events()
+    test_guest_no_events()
+    test_guest_with_events()
+    # add testing for accounts (sending to DB)
