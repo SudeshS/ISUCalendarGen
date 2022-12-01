3892b9279e53166d0944132c53ed86966b2f5063
Xavier <arriagaxavier0@gmail.com>
2022-11-30 19:31:16 -0600

    Small additions to AccountModel and UserModel

--- src/CalendarModel.py
+++ src/CalendarModel.py
@@ -1,4 +1,4 @@
-# Author: Xavier Arriaga
+# Author: Xavier Arriaga, Kaleb Liang
 """
 This is the CalendarModel Class, aka Calendar.
 It is the main class for creating, updating, and removing events.
