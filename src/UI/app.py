dcfe948b433a9754dfead0c582ea459b1b62f682
Kaleb <kaleblianggg@icloud.com>
2022-11-30 22:08:10 -0600

    nothing was changed here

--- src/UI/app.py
+++ src/UI/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager
-import os
 from os.path import join, dirname, realpath
+import os
 
 # login_manager = LoginManager()
 app = Flask(__name__)
@@ -22,16 +22,20 @@ def index():
     return render_template('upload.html')
 
 # Uploading files
+
+
 @app.route("/", methods=['POST'])
 def uploadFiles():
     # get the uploaded file
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        file_path = os.path.join(
+            app.config['UPLOAD_FOLDER'], uploaded_file.filename)
         # set the file path
         uploaded_file.save(file_path)
         # save the file
     return redirect(url_for('index'))
 
+
 if (__name__ == '__main__'):
-    app.run(port = 5000)
+    app.run(port=5000)
