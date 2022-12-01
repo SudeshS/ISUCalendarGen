c88dc48f733ddd584f4727e90e8dfb8149171e48
lucilucifer <jakobosyvertsen@gmail.com>
2022-11-30 21:42:49 -0600

    reuploading lost file

--- src/UI/app.py
+++ src/UI/app.py
@@ -2,19 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager
 import os
 from os.path import join, dirname, realpath
+from flask_sqlalchemy import SQLAlchemy
 
 # login_manager = LoginManager()
 app = Flask(__name__)
 # login_manager.init_app(app)
 
 # enable debugging mode
-app.config["DEBUG"] = True
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:isu@localhost/CalendarDatabase'
+
+db=SQLAlchemy(app)
 
 # Upload Folder
 UPLOAD_FOLDER = app.static_folder
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER+'\\uploads\\'
 
+class CForm(db.Model):
+  __tablename__='calendars'
+  id=db.Column(db.Integer,primary_key=True)
+  file=db.Column(db.Text)
 
+  def __init__(self,file):
+    self.file=file
 # Root URL - What the user connects to
 @app.route('/')
 def index():
@@ -27,11 +36,18 @@ def uploadFiles():
     # get the uploaded file
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        file = uploaded_file
+        cform = CForm(file)
+        db.session.add(cform)
+        db.session.commit()
+        #file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
         # set the file path
-        uploaded_file.save(file_path)
+        #uploaded_file.save(file_path)
         # save the file
     return redirect(url_for('index'))
+
+with app.app_context():
+    db.create_all()
 
 if (__name__ == '__main__'):
     app.run(port = 5000)
--- src/app.py
+++ src/app.py
@@ -1,34 +1,53 @@
-# Author: Xavier Arriaga
 from flask import Flask, render_template, request, url_for, flash, redirect
-##from iCalendar import Calendar, Event
-#from wtforms import Form, BooleanField, StringField, PasswordField, validators
-# ^ for validation if we have time for it, but this requires pip install Flask Flask-WTF
+#from iCalendar import Calendar, Event
+from flask_sqlalchemy import SQLAlchemy
 
 #### NOTE: might have to rename to app.py for it to run properly ####
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cc30d0a491daf6a4ba282e9ea5f9dcfc994cb4b86d66f531'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:isu@localhost/calendarform'
 
-# Messages is just a name, I was gonna switch it to classes
+db=SQLAlchemy(app)
+
+#Messages is just a name, I was gonna switch it to classes
 # but I couldn't get it to work for the time being, so keep it for now
 # unless you know how to fix it
 messages = [
-]
+            ]
+
+class CForm(db.Model):
+  __tablename__='calendars'
+  id=db.Column(db.Integer,primary_key=True)
+  summary=db.Column(db.String())
+  DTStart=db.Column(db.DateTime)
+  DTEnd=db.Column(db.DateTime)
+  Duration=db.Column(db.Time)
+  UNTIL=db.Column(db.DateTime)
+  BYDAY=db.Column(db.String())
+  Description=db.Column(db.Text)
+  Location=db.Column(db.Text)   
+
+  def __init__(self,summary,DTStart,DTEnd,Duration,UNTIL,BYDAY,Description,Location):
+    self.summary=summary
+    self.DTStart=DTStart
+    self.DTEnd=DTEnd
+    self.Duration=Duration
+    self.UNTIL=UNTIL
+    self.BYDAY=BYDAY
+    self.Description=Description
+    self.Location=Location
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html', messages=messages)
-    # these files are key to this working
 
-# Create handles the GET-ing of information from the form
-
-
+#Create handles the GET-ing of information from the form
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         summary = request.form['Summary']
-        DTStart = request.form['DTSTART']  # start date
-        StartTime = request.form['StartTime']  # start time
+        DTStart = request.form['DTSTART']
         DTEnd = request.form['DTEND']
         Duration = request.form['Duration']
         UNTIL = request.form['UNTIL']
@@ -37,12 +56,14 @@ def create():
         Location = request.form['Location']
         # DTStamp will be down on backend, will be time the event is created
 
+        cform=CForm(summary,DTStart,DTEnd,Duration,UNTIL,BYDAY,Description,Location)
+        db.session.add(cform)
+        db.session.commit()
+
         if not summary:
             flash('Class Name is required!')
         elif not DTStart:
-            flash('Start Date is required!')
-        elif not StartTime:
-            flash('Start Time is required')
+            flash('Start Time is required!')
         elif not DTEnd:
             flash('End Time is required')
         elif not Duration:
@@ -52,10 +73,15 @@ def create():
         elif not BYDAY:
             flash('BYDAY is required')
         else:
-            messages.append({'Summary': summary, 'DTSTART': DTStart, 'DTEND': DTEnd, 'Duration': Duration,
-                            'UNTIL': UNTIL, 'BYDAY': BYDAY, 'Description': Description, 'Location': Location})
+            messages.append({'Summary': summary, 'DTSTART': DTStart, 'DTEND': DTEnd, 'Duration': Duration, 'UNTIL': UNTIL, 'BYDAY': BYDAY, 'Description': Description, 'Location': Location})
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
-# Location should be kept not required alongside some others likely (description?)
+#Location should be kept not required alongside some others likely (description?)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
