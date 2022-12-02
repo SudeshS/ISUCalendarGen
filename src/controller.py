fdfb5e3f011420649414971fc74dcb2a376b86f3
Kaleb <kaleblianggg@icloud.com>
2022-12-02 12:46:00 -0600

    added some methods/imports

--- src/controller.py
+++ src/controller.py
@@ -1,8 +1,21 @@
 # Class to handle the displaying of certain messages
+from accountHandler import *
+from UserModel import *
+from CalendarModel import *
+
+
 class Controller:
 
-    def __init__(self, uID):
-        self.userID = uID
+    def __init__(self, id):
+        self.userID = id
 
     def displayMessage():
         pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    calendar = Controller()
+    calendar.main()
