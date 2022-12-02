05c6a10cb53ba95d40a09f6eb025e03ba354b352
Kaleb <kaleblianggg@icloud.com>
2022-12-02 12:44:58 -0600

    merge

--- src/app.py
+++ src/app.py
@@ -1,92 +1,59 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
-#from iCalendar import Calendar, Event
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager
+import os
+from os.path import join, dirname, realpath
 from flask_sqlalchemy import SQLAlchemy
 
-#### NOTE: might have to rename to app.py for it to run properly ####
+# login_manager = LoginManager()
 app = Flask(__name__)
+# login_manager.init_app(app)
+
+# enable debugging mode
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:isu@localhost/test'
 
 db = SQLAlchemy(app)
 
-# Messages is just a name, I was gonna switch it to classes
-# but I couldn't get it to work for the time being, so keep it for now
-# unless you know how to fix it
-messages = [
-]
+# Upload Folder
+UPLOAD_FOLDER = app.static_folder
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER+'\\uploads\\'
 
 
 class CForm(db.Model):
     __tablename__ = 'calendar'
     id = db.Column(db.Integer, primary_key=True)
-    summary = db.Column(db.String())
-    DTStart = db.Column(db.DateTime)
-    DTEnd = db.Column(db.DateTime)
-    Duration = db.Column(db.Time)
-    UNTIL = db.Column(db.DateTime)
-    BYDAY = db.Column(db.String())
-    Description = db.Column(db.Text)
-    Location = db.Column(db.Text)
+    file = db.Column(db.Text)
 
-    def __init__(self, summary, DTStart, DTEnd, Duration, UNTIL, BYDAY, Description, Location):
-        self.summary = summary
-        self.DTStart = DTStart
-        self.DTEnd = DTEnd
-        self.Duration = Duration
-        self.UNTIL = UNTIL
-        self.BYDAY = BYDAY
-        self.Description = Description
-        self.Location = Location
+    def __init__(self, file):
+        self.file = file
+# Root URL - What the user connects to
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    # Set the upload HTML template '\templates\index.html'
+    return render_template('upload.html')
 
-# Create handles the GET-ing of information from the form
+# Uploading files
 
 
-@app.route('/create/', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        summary = request.form['Summary']
-        DTStart = request.form['DTSTART']
-        DTEnd = request.form['DTEND']
-        Duration = request.form['Duration']
-        UNTIL = request.form['UNTIL']
-        BYDAY = request.form['BYDAY']
-        Description = request.form['Description']
-        Location = request.form['Location']
-        # DTStamp will be down on backend, will be time the event is created
-
-        cform = CForm(summary, DTStart, DTEnd, Duration,
-                      UNTIL, BYDAY, Description, Location)
+@app.route("/", methods=['POST'])
+def uploadFiles():
+    # get the uploaded file
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        file = uploaded_file
+        cform = CForm(file)
         db.session.add(cform)
         db.session.commit()
-
-        if not summary:
-            flash('Class Name is required!')
-        elif not DTStart:
-            flash('Start Time is required!')
-        elif not DTEnd:
-            flash('End Time is required')
-        elif not Duration:
-            flash('Duration is required')
-        elif not UNTIL:
-            flash('UNTIL is required')
-        elif not BYDAY:
-            flash('BYDAY is required')
-        else:
-            messages.append({'Summary': summary, 'DTSTART': DTStart, 'DTEND': DTEnd, 'Duration': Duration,
-                            'UNTIL': UNTIL, 'BYDAY': BYDAY, 'Description': Description, 'Location': Location})
-            return redirect(url_for('index'))
-
-    return render_template('create.html')
-
-# Location should be kept not required alongside some others likely (description?)
+        #file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        # set the file path
+        # uploaded_file.save(file_path)
+        # save the file
+    return redirect(url_for('index'))
 
 
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if (__name__ == '__main__'):
+    app.run(port=5000)
