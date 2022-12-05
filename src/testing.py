6595ab895f1fcf3772dd5f8562745632164e4b62
Kaleb <kaleblianggg@icloud.com>
2022-12-05 17:11:10 -0600

    added simple addevent and removeevent test

--- src/testing.py
+++ src/testing.py
@@ -19,7 +19,7 @@ def test_parse_no_events():   # guest test parse calendar with no events
 
 # test 2
 def test_guest_with_events():  # guest test parse calendar with four events
-    f_path = 'data/input_isu_cal_iii.ics'
+    f_path = 'UI/static/uploads/test_calendar.ics'
     test_cal = CalendarModel.parse_cal(f_path)
 
     if CalendarModel.checkCalendarFormat(test_cal):
@@ -29,6 +29,22 @@ def test_guest_with_events():  # guest test parse calendar with four events
         print(test_var)
     else:
         print("Format Error")
+
+
+def test_add_events():
+    messages = [
+    {'Summary': 'asdf', 'StartDate': '11/12/2022', 'StartTime': '11:00AM', 'Duration': '1H00M', 'UNTIL': '12/12/2022', 'BYDAY': 'FR', 'Description': 'ewofn132n', 'Location': '12r3'}, {'Summary': 'asdfg', 'StartDate': '09/12/2022', 'StartTime': '12:00PM', 'Duration': '1H15M', 'UNTIL': '12/15/2022', 'BYDAY': 'MO', 'Description': '21on241', 'Location': '12241'}
+    ]
+
+    event = CalendarModel.addEvents(list(messages))
+
+def test_remove_events():
+    messages = [
+    {'Summary': 'COM 223', 'StartDate': '11/12/2022', 'StartTime': '11:00AM', 'Duration': '1H00M', 'UNTIL': '12/12/2022', 'BYDAY': 'FR', 'Description': 'ewofn132n', 'Location': '12r3'}, {'Summary': 'asdfg', 'StartDate': '09/12/2022', 'StartTime': '12:00PM', 'Duration': '1H15M', 'UNTIL': '12/15/2022', 'BYDAY': 'MO', 'Description': '21on241', 'Location': '12241'}
+    ]
+    filename = 'UI/static/uploads/test_calendar.ics'
+
+    CalendarModel.removeEvents(filename, list(messages[0].values()))
 
 
 # test 3
@@ -45,6 +61,8 @@ def test_guest_add_events():   # guest test add events
 
 
 if __name__ == "__main__":
-    test_parse_no_events()
-    test_guest_with_events()
+    # test_parse_no_events()
+    # test_guest_with_events()
+    # test_add_events()
+    test_remove_events()
     # add testing for accounts (sending to DB)
