5066745f90583319832d3c43349438d9f14e0d0a
Sudesh Sahu <sahu.k.sudesh@gmail.com>
2022-12-05 17:03:13 -0600

    Login, logout, and create an account is all working properly.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
@@ -7,7 +8,10 @@ from accountHandler import AccountHandler as User
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cc30d0a491daf6a4ba282e9ea5f9dcfc994cb4b86d66f531'
+
+# load environment variables from .env file
+load_dotenv()
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["DEBUG"] = True
 
 login_manager = LoginManager()
@@ -20,28 +24,33 @@ UPLOAD_FOLDER = app.static_folder
 app.config['UPLOAD_FOLDER'] = f"{UPLOAD_FOLDER}/uploads" # For macos/linux
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER+'\\uploads\\'
 
-# Database config, loads environment variables from .env file (don't put that file in github)
-load_dotenv()
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True)
     password = db.Column(db.Text)
     calendars = db.relationship('Calendar', backref='user')
     
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Calendar(db.Model):
   cal_id = db.Column(db.Integer, primary_key=True)
   filename = db.Column(db.Text)
   file_data = db.Column(db.Text)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
   def __init__(self, filename, file_data, user_id):
     self.filename = filename
@@ -58,41 +67,69 @@ with app.app_context():
 # Login (on connection)
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('login.html'))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    error = None
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
 
+    error = None
     if request.method == 'POST':
         uname = request.form['username']
         pword = request.form['password']
-        user1 = User(uname, pword)
-        if not user1.login():
-            error = 'Invalid Credentials. Please try again or create a new Account!'
+        user = User.query.filter_by(username=uname).first()
+
+        if user and user.check_password(pword):
+            login_user(user)
+            return render_template('home.html', current_user=current_user)
         else:
-            return redirect(url_for('home'))
+            error = 'Invalid Credentials. Please try again or create a new account'
+            flash(error)
+            return redirect(url_for('login'))
     
-    return render_template('login.html', error=error)
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/create_account/', methods=['GET', 'POST'])
 def create_account():
     error = None
-
     if request.method == 'POST':
         uname = request.form['username']
         pword1 = request.form['password1']
         pword2 = request.form['password2']
         if pword1 != pword2:
             error = 'Please make sure the passwords match.'
+            flash(error)
         else:
             # **************SAVE TO DATABASE THE NEW USER HERE**********************
-            
-            return redirect(url_for('home'))
+            existing_user = User.query.filter_by(username=uname).first()
+            if existing_user is None:
+                user = User(uname, pword1)
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                return render_template('home.html', current_user=current_user)
+            flash('Error: A user already exists with that username.')
 
-    return render_template('createAccount.html', error=error)
+    return render_template('createAccount.html')
+
 
 @app.route('/home/')
 def home():
