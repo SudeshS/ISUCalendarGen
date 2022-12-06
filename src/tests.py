f3e896a3c01892bb04adc1e9bf142cfad2d0a8cb
Gordon Blankenship <gordon.l.blankenship@gmail.com>
2022-12-06 01:12:00 -0600

    added tests file

---
+++ src/tests.py
@@ -0,0 +1,28 @@
+# testing file for all class and files used.
+import pytest
+import accountHandler, AccountModel
+import CalendarModel, CalendarPreview
+import DatabaseConnection, DatabaseService
+import eventModel, ics_generator
+import server, UserModel
+
+# accountHandler tests
+def accountHandlerTest():
+    
+# AccountModel tests
+
+# CalendarModel tests
+
+# CalendarPreview tests
+
+# DatabaseConnection tests
+
+# DatabaseService tests
+
+# eventModel tests
+
+# ics_generator tests
+
+# server tests
+
+# UserModel tests
